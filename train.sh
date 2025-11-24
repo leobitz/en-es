66443393b6e777be6ea6917faf6c672630ba459a
Amanuel Negash <amanyamy@gmail.com>
2025-11-24 23:16:30 +0300

    fix epochs

--- train.sh
+++ train.sh
@@ -1,12 +1,11 @@
-python train.py --epochs 10 --num-frozen-layers 6 --num-task-layers 0 --use-lora
-python train.py --epochs 10 --num-frozen-layers 10 --num-task-layers 0 --use-lora
-python train.py --epochs 10 --num-frozen-layers 16 --num-task-layers 0 --use-lora
-python train.py --epochs 10 --num-frozen-layers 22 --num-task-layers 0 --use-lora
-python train.py --epochs 10 --num-frozen-layers 28 --num-task-layers 0 --use-lora
-
-# python train.py --epochs 10 --num-frozen-layers 6 --num-task-layers 2
-# python train.py --epochs 10 --num-frozen-layers 10 --num-task-layers 2
-# python train.py --epochs 10 --num-frozen-layers 16 --num-task-layers 2
+python train.py --epochs 15 --num-frozen-layers 6 --num-task-layers 2
+python train.py --epochs 15 --num-frozen-layers 10 --num-task-layers 2
+python train.py --epochs 15 --num-frozen-layers 16 --num-task-layers 2
 # python train.py --epochs 10 --num-frozen-layers 22 --num-task-layers 2
 # python train.py --epochs 10 --num-frozen-layers 28 --num-task-layers 2
 
+# python train.py --epochs 10 --num-frozen-layers 6 --num-task-layers 0 --use-lora
+# python train.py --epochs 10 --num-frozen-layers 10 --num-task-layers 0 --use-lora
+# python train.py --epochs 10 --num-frozen-layers 16 --num-task-layers 0 --use-lora
+# python train.py --epochs 10 --num-frozen-layers 22 --num-task-layers 0 --use-lora
+# python train.py --epochs 10 --num-frozen-layers 28 --num-task-layers 0 --use-lora
